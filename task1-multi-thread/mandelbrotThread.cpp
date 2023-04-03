9b471e260e5ad5482625370b7e9d1c5ca2c6b60d
SSK015 <18252677381@163.com>
2023-04-03 23:17:54 +0800

    doing1p/4x

--- task1-multi-thread/mandelbrotThread.cpp
+++ task1-multi-thread/mandelbrotThread.cpp
@@ -34,8 +34,11 @@ void workerThreadStart(WorkerArgs * const args) {
     // to compute a part of the output image.  For example, in a
     // program that uses two threads, thread 0 could compute the top
     // half of the image and thread 1 could compute the bottom half.
-
-    printf("Hello world from thread %d\n", args->threadId);
+	for (int i = args->threadId; i < (int)args->height; i += args->numThreads) {
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width,
+                         args->height, i, 1, args->maxIterations, args->output);
+    }
+//    printf("Hello world from thread %d\n", args->threadId);
 }
 
 //
